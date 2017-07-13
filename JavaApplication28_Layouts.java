a3c52ba95ec825e8803bc4cc11c02658ee7f374f
Espadaclar <eneroborde8002@gmail.com>
2017-07-13 10:17:09 +0200

    Layout BorderLayout constructor sin parametros. vd 82.

--- JavaApplication28_Layouts.java
+++ JavaApplication28_Layouts.java
@@ -22,36 +22,33 @@ public class JavaApplication28_Layouts extends JFrame{
         setBounds(100, 100, 890, 450);
         setTitle("Ventana para Layout");
         setVisible(true);
-        
+
         Toolkit ic = Toolkit.getDefaultToolkit();
         Image icono = ic.getImage("../icono2.gif");
         setIconImage(icono);        
     }
-    
+
     private class Lamina extends JPanel{
 
-        public Lamina() {           
-            add(new JButton("Azul"));
-            add(new JButton("Rojo"));
-            add(new JButton("Amarillo"));            
+        public Lamina() { 
+            // 1º Para aplicar la disposicon o Layout 'BorderLayout()' instanciamos la clase directamente en 
+            //    el mt setLayout(...), utilizando en este caso el constructor por defecto.
+            setLayout(new BorderLayout());
+            // 2º tenemos que indicar a nuestros botones en que zona han de ubicarse N, S, E, O o en el CENTRO
+            //    el mt, add() de la clase JPanel, admite un 2º argumento, que es la posicion que utiliza el componente.
+            add(new JButton("Azul"),  BorderLayout.NORTH);
+            add(new JButton("Rojo"), BorderLayout.SOUTH);
+            add(new JButton("Amarillo"), BorderLayout.WEST);
+            add(new JButton("Verde"), BorderLayout.CENTER);
+            add(new JButton("Anaranjado"), BorderLayout.EAST);  
             //asigna la posicion de los objetos.
-            setLayout(new FlowLayout(FlowLayout.CENTER, 80, 100));
+            //setLayout(new FlowLayout(FlowLayout.CENTER, 80, 100));
+            // setLayout(new BorderLayout());
         }
-        
+
     }    
-    
+
 }
-
-
-
-
-
-
-
-
-
-
-
 
 
 
